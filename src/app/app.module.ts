3e1fb654a4b43bcf7f3421283938319af529f129
Benjamin Komen <komenb@gmail.com>
2018-09-30 16:14:47 +0200

    starting with pieces

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BoardComponent } from './board/board.component';
+import { PiecesComponent } from './pieces/pieces.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BoardComponent
+    BoardComponent,
+    PiecesComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/pieces/pieces.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-pieces',
+  templateUrl: './pieces.component.html',
+  styleUrls: ['./pieces.component.css']
+})
+export class PiecesComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
